be7921aa7c8a9647f9c965717f4eb8edd6e4271b
Oscar Fuentes <oskar4416@gmail.com>
2016-05-14 20:47:39 +0200

    Codigo refactorizado + Documentacion añadida

--- src/net/iessanclemente/a13oscarfm/katafizzbuzz/KataFizzBuzz.java
+++ src/net/iessanclemente/a13oscarfm/katafizzbuzz/KataFizzBuzz.java
@@ -2,6 +2,13 @@ package net.iessanclemente.a13oscarfm.katafizzbuzz;
 
 public class KataFizzBuzz {
 	
+	/**
+	 * This method check if the given number is multiple of 3, 5 or both
+	 * and returns 'Fizz', 'Buzz' or 'FizzBuzz' respectively.
+	 * 
+	 * @param num: The given number
+	 * @return 'Fizz'|'Buzz'|'FizzBuzz'
+	 */
 	public String multiplos(int num){
 		String resp = "";
 		if(num%3 == 0 && num%5 == 0){
--- tests/net/iessaclemente/a13oscarfm/katafizzbuzz/tests/TestKataFizzBuzz.java
+++ tests/net/iessaclemente/a13oscarfm/katafizzbuzz/tests/TestKataFizzBuzz.java
@@ -9,29 +9,41 @@ import net.iessanclemente.a13oscarfm.katafizzbuzz.KataFizzBuzz;
 
 public class TestKataFizzBuzz {
 
+	/**
+	 * A global variable for all tests
+	 */
 	private KataFizzBuzz kfb;
 	
+	/**
+	 * Initialise kfb before start tests
+	 */
 	@Before
 	public void setUp(){
 		kfb = new KataFizzBuzz();
 	}
 	
+	/**
+	 * This test check if the given number is multiple of 3
+	 */
 	@Test
 	public void testMultiplposTres() {
 		String result = kfb.multiplos(9);
 		assertEquals("Fizz", result);
 	}
-	
+	/**
+	 * This test check if the given number is multiple of 5
+	 */
 	@Test
 	public void testMultiplposCinco() {
 		String result = kfb.multiplos(25);
 		assertEquals("Buzz", result);
 	}
-	
+	/**
+	 * This test check if the given number is multiple of 3 and 5
+	 */
 	@Test
 	public void testMultiplosAmbos(){
 		String result = kfb.multiplos(15);
 		assertEquals("FizzBuzz", result);
 	}
-
 }
