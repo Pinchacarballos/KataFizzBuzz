86bd52fe39e821b0649a89f0583e55da7674e94e
Oscar Fuentes <oskar4416@gmail.com>
2016-05-14 19:34:36 +0200

    Multiplos de tres

--- src/net/iessanclemente/a13oscarfm/katafizzbuzz/KataFizzBuzz.java
+++ src/net/iessanclemente/a13oscarfm/katafizzbuzz/KataFizzBuzz.java
@@ -1,5 +1,11 @@
 package net.iessanclemente.a13oscarfm.katafizzbuzz;
 
 public class KataFizzBuzz {
-
+	public String multiplosTres(int num){
+		String resp = "";
+		if(num%3 == 0){
+			resp = "Fizz";
+		}
+		return resp;
+	}
 }
--- tests/net/iessaclemente/a13oscarfm/katafizzbuzz/tests/TestKataFizzBuzz.java
+++ tests/net/iessaclemente/a13oscarfm/katafizzbuzz/tests/TestKataFizzBuzz.java
@@ -2,13 +2,24 @@ package net.iessaclemente.a13oscarfm.katafizzbuzz.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import net.iessanclemente.a13oscarfm.katafizzbuzz.KataFizzBuzz;
 
 public class TestKataFizzBuzz {
 
+	private KataFizzBuzz kfb;
+	
+	@Before
+	public void setUp(){
+		kfb = new KataFizzBuzz();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMultiplposTres() {
+		String result = kfb.multiplosTres(9);
+		assertEquals("Fizz", result);
 	}
 
 }
